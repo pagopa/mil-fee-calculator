e8b2add8c08f13e9c7ac312686621213ad2ca6d3
fabrizio.guerrini <fabrizio.guerrini@C11-C9GE23PFRDO.dir.svc.accenture.com>
2023-05-11 09:35:53 +0200

    PSCF2-76 Migrazione mil-fee-calculator a Quarkus 3.0
    PSCF2-83 Migrazione package mil-fee-calculator
    PSCF2-70 Modifica Fee Calculator Service per adeguamento a nuova request
    GEC

---
+++ src/main/java/it/pagopa/swclient/mil/feecalculator/bean/Notice.java
@@ -0,0 +1,96 @@
+
+package it.pagopa.swclient.mil.feecalculator.bean;
+
+import java.util.List;
+
+import it.pagopa.swclient.mil.feecalculator.ErrorCode;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+
+/**
+ * Details of a payment notice
+ */
+public class Notice {
+	
+	/**
+	 * Amount in euro cents
+	 */
+	@NotNull(message = "[" + ErrorCode.AMOUNT_MUST_NOT_BE_NULL + "] amount must not be null")
+	@Min(value = 1, message = "[" + ErrorCode.AMOUNT_EXCEEDED_MIN_VALUE + "] amount must not be less than {value}" )
+	@Max(value = 99999999999L, message = "[" + ErrorCode.AMOUNT_EXCEED_MAX_VALUE + "] must must not be greater than {value}" )
+	private Long amount;
+	
+	/**
+	 * Tax code of the creditor company
+	 */
+	@NotNull(message = "[" + ErrorCode.PA_TAX_CODE_MUST_NOT_BE_NULL + "] paTaxCode must not be null")
+	@Pattern(regexp = "^\\d{11}$", message = "[" + ErrorCode.PA_TAX_CODE_MUST_MATCH_REGEXP + "] paxCode must match \"{regexp}\"")
+	private String paTaxCode;
+	
+	/**
+	 * 	Transfer essential data
+	 */
+	@Valid
+	@NotNull(message = "[" + ErrorCode.TRANSFERS_MUST_NOT_BE_NULL + "] transfers must not be null")
+	@Size(max = 5, message = "[" + ErrorCode.TRANSFERS_EXCEEDED_MAX_VALUE + "] transfers must contain at most {max} elements")
+	private List<Transfer> transfers;
+	
+	/**
+	 * @return the amount
+	 */
+	public Long getAmount() {
+		return amount;
+	}
+
+	/**
+	 * @param amount the amount to set
+	 */
+	public void setAmount(Long amount) {
+		this.amount = amount;
+	}
+
+	/**
+	 * @return the paTaxCode
+	 */
+	public String getPaTaxCode() {
+		return paTaxCode;
+	}
+
+	/**
+	 * @param paTaxCode the paTaxCode to set
+	 */
+	public void setPaTaxCode(String paTaxCode) {
+		this.paTaxCode = paTaxCode;
+	}
+
+	/**
+	 * @return the transfers
+	 */
+	public List<Transfer> getTransfers() {
+		return transfers;
+	}
+
+	/**
+	 * @param transfers the transfers to set
+	 */
+	public void setTransfers(List<Transfer> transfers) {
+		this.transfers = transfers;
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("Notice [amount=");
+		builder.append(amount);
+		builder.append(", paTaxCode=");
+		builder.append(paTaxCode);
+		builder.append(", transfers=");
+		builder.append(transfers);
+		builder.append("]");
+		return builder.toString();
+	}
+}
---
+++ src/test/java/it/pagopa/swclient/mil/feecalculator/it/IntegrationTestProfile.java
@@ -0,0 +1,45 @@
+package it.pagopa.swclient.mil.feecalculator.it;
+
+import com.google.common.collect.ImmutableList;
+import io.quarkus.test.junit.QuarkusTestProfile;
+import it.pagopa.swclient.mil.feecalculator.it.resource.WiremockTestResource;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class IntegrationTestProfile implements QuarkusTestProfile {
+
+    @Override
+    public Map<String, String> getConfigOverrides() {
+
+        Map<String, String> configOverrides = new HashMap<>();
+
+        configOverrides.put("feecalculator.quarkus-log-level", "DEBUG");
+        configOverrides.put("feecalculator.app-log-level", "DEBUG");
+
+        configOverrides.put("fees.service.connect-timeout", "3000");
+        configOverrides.put("fees.service.read-timeout", "3000");
+        configOverrides.put("ocp-apim-subscription", "abcde");
+
+        configOverrides.put("mil.rest-service.connect-timeout", "3000");
+        configOverrides.put("mil.rest-service.read-timeout", "3000");
+        configOverrides.put("mil.rest-service.subscription-key", "abcde");
+        configOverrides.put("mil.acquirer-conf.version", "1.0.0");
+
+        return configOverrides;
+    }
+
+    @Override
+    public List<TestResourceEntry> testResources() {
+        return ImmutableList.of(
+                new TestResourceEntry(WiremockTestResource.class)
+        );
+    }
+
+    @Override
+    public boolean disableGlobalTestResources() {
+        return true;
+    }
+
+}
