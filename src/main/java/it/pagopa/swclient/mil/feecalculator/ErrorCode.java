e1b8364bb1e85811360d5891c28689e85d24c410
RiccardoGiuliani <144138935+RiccardoGiuliani@users.noreply.github.com>
2023-11-13 10:30:36 +0100

    fix: Fix postman collection for optional paymentMethod [SCS-290]  (#11)

--- src/main/java/it/pagopa/swclient/mil/feecalculator/ErrorCode.java
+++ src/main/java/it/pagopa/swclient/mil/feecalculator/ErrorCode.java
@@ -10,7 +10,6 @@ public final class ErrorCode {
 	public static final String MODULE_ID 										= "007";
 	
 	public static final String PAYMENT_METHOD_MUST_MATCH_REGEXP 				= MODULE_ID + "000001";
-	public static final String PAYMENT_METHOD_MUST_NOT_BE_NULL 					= MODULE_ID + "000002";
 	
 	public static final String NOTICES_MUST_NOT_BE_NULL							= MODULE_ID + "000003";
 	public static final String NOTICES_LIST_EXCEEDED_SIZE 						= MODULE_ID + "000004";
@@ -26,7 +25,6 @@ public final class ErrorCode {
 	public static final String TRANSFERS_EXCEEDED_MAX_VALUE						= MODULE_ID + "00000B";
 	
 	public static final String ERROR_RETRIEVING_FEES							= MODULE_ID + "00000C";
-	public static final String ERROR_RETRIEVING_ID_PSP							= MODULE_ID + "00000D";
 	
 	public static final String TRANSFERS_PA_TAX_CODE_MUST_NOT_BE_NULL 			= MODULE_ID + "00000E";
 	public static final String TRANSFERS_PA_TAX_CODE_MUST_MATCH_REGEXP 			= MODULE_ID + "00000F";
